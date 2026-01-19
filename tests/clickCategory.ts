6d14712a400f4750d0fd7a15a004daa451ce4785
kgagulic <kgagulic@ffos.hr>
2026-01-19 15:22:34 +0100

    "feat: Add HomePage POM + ClickCategory test"

---
+++ tests/clickCategory.ts
@@ -0,0 +1,24 @@
+import { test } from '@playwright/test';
+import { HomePage } from '../POMs/homePage';
+let homePage: HomePage;
+
+test('navigate category', async ({ page }) => {
+  const homePage = new HomePage(page);
+  await page.goto('https://www.next.ro/en/branded');
+  await homePage.goToCategory();
+
+});
+
+
+/*
+test('Add item to cart', async () => {
+    // given
+    await homePage.addItemToCart('Phones', 'Samsung galaxy s6');
+    await homePage.assertItemAddedToCart('Product added');
+    // when
+    await cartPage.openCart();
+    await cartPage.assertCartIsOpened();
+    // then
+    await cartPage.assertItemIsInCart('Samsung galaxy s6');
+});
+*/
